ab9ef9599fb454cb044fe8b096b0d5f169461e3e
vvdf <t42please@gmail.com>
2020-09-01 03:07:44 -0700

    INIT base page and react functionality

---
+++ client/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+        ALLONS-Y, ALLONSO!
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const ENTRY_FILE = path.join(__dirname, 'client', 'src', 'index.jsx');
+const OUT_DIR = path.join(__dirname, 'client', 'dist');
+// const SRC_DIR = path.join(__dirname, 'client', 'src');
+
+module.exports = {
+  mode: 'development',
+  entry: ENTRY_FILE,
+  output: {
+    path: OUT_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
