d58228576eb6673b872aa822290f088801c433f2
vvdf <t42please@gmail.com>
2020-09-04 00:54:38 -0700

    ADD basic demo of isometric tile rendering

--- client/src/components/Engine.js
+++ client/src/components/Engine.js
@@ -22,14 +22,57 @@ class Engine {
         this.loader.resources['assets/96x48_rally_police_girl.png'].texture,
       );
 
-      sprite.x = 300;
-      sprite.y = 200;
+      sprite.x = 355;
+      sprite.y = 208;
 
+      const ground = PIXI.utils.TextureCache['assets/64x32_floor_tiles.png'];
+      ground.frame = new PIXI.Rectangle(64, 0, 64, 32);
+      const road = PIXI.utils.TextureCache['assets/64x32_floor_tiles.png'];
+      road.frame = new PIXI.Rectangle(64, 0, 64, 32);
+
+      // refactor this logic into a grid->isometric converter helper methods
+      const map = [
+        ['g', 'g', 'g', 'g', 'g'],
+        ['g', 'g', 'g', 'g', 'g'],
+        ['r', 'r', 'r', 'r', 'r'],
+        ['g', 'g', 'g', 'g', 'g'],
+        ['g', 'g', 'g', 'g', 'g'],
+      ];
+
+      // up right one is (+32, -16)
+
+      const stageMap = () => {
+        const startX = 350;
+        const startY = 216;
+
+        for (let i = 0; i < map.length; i++) {
+          let y = startY + i * 16;
+          for (let j = 0; j < map[i].length; j++) {
+            let x = startX - i * 32;
+            let tile;
+            if (map[i][j] === 'g') {
+              console.log('ground found');
+              tile = new PIXI.Sprite(ground);
+            } else if (map[i][j] === 'r') {
+              console.log('road found');
+              tile = new PIXI.Sprite(road);
+            } else {
+              console.log('neither found', map[i][j]);
+            }
+            tile.x = x + j * 32;
+            tile.y = y + j * 16;
+            this.game.stage.addChild(tile);
+          }
+        }
+      };
+
+      stageMap();
       this.game.stage.addChild(sprite);
     };
 
     this.loader
       .add('assets/96x48_rally_police_girl.png')
+      .add('assets/64x32_floor_tiles.png')
       .load(setup);
   }
 }
