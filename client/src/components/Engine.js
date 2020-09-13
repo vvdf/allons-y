e2d87c63ef83adbcfdb3a25f460dcbe093363e35
vvdf <t42please@gmail.com>
2020-09-13 04:51:18 -0700

    ADD utility functions and handlers for rendering of map and entities

--- client/src/components/Engine.js
+++ client/src/components/Engine.js
@@ -1,17 +1,34 @@
 import * as PIXI from 'pixi.js';
+import GameMap from './GameMap';
 
 class Engine {
   constructor(targetEle) {
     // initialize game renderer, append as child to passed in element
-    const game = new PIXI.Application({
+    this.settings = {
       backgroundColor: 0xf0c5e9,
       width: 800,
-      height: 500,
+      height: 550,
       resolution: 1,
-    });
+    };
+
+    this.constants = {
+      // these include pixel overlap
+      tileWidth: 28,
+      tileHeight: 14,
+      tileDepth: 10,
+    };
+
+    const game = new PIXI.Application(this.settings);
 
     this.game = game;
     this.state = this.play;
+    this.textures = {};
+    this.entityList = {};
+    this.player = {
+      x: 0,
+      y: 0,
+    };
+    this.gameMap = GameMap(10, 10, 'g');
     this.loader = PIXI.Loader.shared;
     targetEle.appendChild(game.view);
     this.init();
@@ -21,72 +38,15 @@ class Engine {
     // initialize game Engine variables/systems/assets
     // create and populate scenes
     const setup = () => {
-      const sprite = new PIXI.Sprite(
-        this.loader.resources['assets/96x48_rally_police_girl.png'].texture,
-      );
+      this.textures.player = this.loader.resources['assets/96x48_rally_police_girl.png'].texture;
+      this.textures.grass = PIXI.utils.TextureCache['grass.png'];
+      this.textures.road = PIXI.utils.TextureCache['road.png'];
+      this.textures.water = PIXI.utils.TextureCache['water1.png'];
 
-      sprite.x = 358;
-      sprite.y = 200;
-
-      const ground = PIXI.utils.TextureCache['grass.png'];
-      const road = PIXI.utils.TextureCache['road.png'];
-      const water = PIXI.utils.TextureCache['water1.png'];
-
-      // refactor this logic into a grid->isometric converter helper methods
-      const map = [
-        ['g', 'g', 'g', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g'],
-        ['g', 'g', 'g', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g'],
-        ['r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r'],
-        ['g', 'g', 'g', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g'],
-        ['g', 'g', 'w', 'w', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g'],
-        ['w', 'w', 'w', 'w', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g'],
-        ['g', 'w', 'w', 'w', 'g', 'w', 'g', 'g', 'w', 'g', 'g', 'w', 'g', 'g', 'w', 'g', 'g', 'w', 'g'],
-        ['g', 'g', 'g', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g'],
-        ['g', 'g', 'w', 'w', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g'],
-        ['w', 'w', 'w', 'w', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g'],
-        ['g', 'g', 'g', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g'],
-        ['g', 'g', 'w', 'w', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g'],
-        ['w', 'w', 'w', 'w', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g'],
-        ['g', 'w', 'w', 'w', 'g', 'w', 'g', 'g', 'w', 'g', 'g', 'w', 'g', 'g', 'w', 'g', 'g', 'w', 'g'],
-        ['g', 'g', 'g', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g', 'g', 'r', 'g'],
-        ['g', 'g', 'w', 'w', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g'],
-        ['w', 'w', 'w', 'w', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g', 'w', 'w', 'g'],
-      ];
-
-      // up right one is (+32, -16)
-
-      const stageMap = () => {
-        const startX = 350;
-        const startY = 216;
-
-        for (let i = 0; i < map.length; i++) {
-          let y = startY + i * 14;
-          for (let j = 0; j < map[i].length; j++) {
-            let x = startX - i * 28;
-            let tile;
-            if (map[i][j] === 'g') {
-              console.log('ground found');
-              tile = new PIXI.Sprite(ground);
-            } else if (map[i][j] === 'r') {
-              console.log('road found');
-              tile = new PIXI.Sprite(road);
-            } else if (map[i][j] === 'w') {
-              console.log('water found');
-              tile = new PIXI.Sprite(water);
-            } else {
-              console.log('neither found', map[i][j]);
-            }
-            tile.x = x + j * 28;
-            tile.y = y + j * 14;
-            this.game.stage.addChild(tile);
-          }
-        }
-      };
-
-      stageMap();
-      this.game.stage.addChild(sprite);
+      this.render();
     };
 
+    // load assets into renderer and then run renderer setup using loaded assets
     this.loader
       .add('assets/96x48_rally_police_girl.png')
       .add('assets/643212_floor_tiles.json')
@@ -100,6 +60,62 @@ class Engine {
   play(delta) {
     // play state function
 
+  }
+
+  gridToViewX(sprite, dx = 0, dy = 0) {
+    // convert grid X position on map to view X position on renderer, factoring iso coords
+    // dx and dy are deltas between target grid pos, and center (aka player)
+    return (this.settings.width / 2) - (sprite.width / 2)
+      + (this.constants.tileWidth * dx) - (this.constants.tileWidth * dy);
+  }
+
+  gridToViewY(sprite, dx = 0, dy = 0, isEntity = false) {
+    // convert grid Y position on map to view Y position on renderer, factoring iso coords
+    // dx and dy are deltas between target grid pos, and center (aka player)
+    if (isEntity) {
+      return (this.settings.height / 2) - (sprite.height)
+        + (this.constants.tileHeight * dx) + (this.constants.tileHeight * dy);
+    }
+    return (this.settings.height / 2) - (sprite.height / 2)
+      + (this.constants.tileHeight * dx) + (this.constants.tileHeight * dy);
+  }
+
+  render() {
+    this.renderMap();
+    this.renderEntities();
+  }
+
+  renderMap() {
+    const mapSprite = new PIXI.Container();
+    for (let y = 0; y < this.gameMap.length; y += 1) {
+      for (let x = 0; x < this.gameMap[y].length; x += 1) {
+        const dy = y - this.player.y;
+        const dx = x - this.player.x;
+        let tile;
+        if (this.gameMap[y][x] === 'g') {
+          tile = new PIXI.Sprite(this.textures.grass);
+        } else if (this.gameMap[y][x] === 'r') {
+          tile = new PIXI.Sprite(this.textures.road);
+        } else if (this.gameMap[y][x] === 'w') {
+          tile = new PIXI.Sprite(this.textures.water);
+        }
+
+        tile.x = this.gridToViewX(tile, dx, dy);
+        tile.y = this.gridToViewY(tile, dx, dy);
+
+        mapSprite.addChild(tile);
+      }
+    }
+
+    this.game.stage.addChild(mapSprite);
+  }
+
+  renderEntities() {
+    const playerSprite = new PIXI.Sprite(this.textures.player);
+    playerSprite.x = this.gridToViewX(playerSprite);
+    playerSprite.y = this.gridToViewY(playerSprite, 0, 0, true);
+
+    this.game.stage.addChild(playerSprite);
   }
 
   addToScene(spritesArr) {
