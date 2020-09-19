514d55a69954257c6cb5d276db013c9f9760074e
vvdf <t42please@gmail.com>
2020-09-19 04:52:25 -0700

    IMPLEMENT barebones network functionality

--- client/src/components/Engine.js
+++ client/src/components/Engine.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import GameMap from './GameMap';
 import Entity from './Entity';
 import Input from './Input';
@@ -26,8 +27,8 @@ class Engine {
     this.entities = [];
     this.entityNextId = 0;
     this.entityIdMap = {};
-    this.gameMap = new GameMap(50, 50, 'w');
-    // this.gameMap.load();
+    this.gameMap = new GameMap(50, 50, 'g');
+    this.gameMap.load();
     this.input = new Input(this.eventQueue);
     this.renderer = new Renderer(this.settings, this.constants, this.entities, this.gameMap);
     targetEle.appendChild(this.renderer.getView());
@@ -38,8 +39,29 @@ class Engine {
   init() {
     // initialize game Engine variables/systems/assets
     this.createEntity('Camera', 'blank'); // create game camera as focus entity
-    this.createEntity('Apron', 'player'); // create player entity
-    this.renderer.addToTicker((delta) => this.gameLoop(delta));
+    this.createEntity(`Apron${Math.floor(Math.random() * 1000)}`, 'player'); // create player entity
+    axios.get('/entity')
+      .then((response) => {
+        if (response.data[0] && response.data[0].x && response.data[0].y) {
+          console.log('TOTAL ENTITIES RECEIVED: ', response.data.length);
+          this.entities[1].setPos(response.data[0].x, response.data[0].y);
+          this.centerCamera(false);
+        } else {
+          axios.post('/entity', { ...this.entities[1], map: null });
+        }
+        for (let i = 1; i < response.data.length; i += 1) {
+          console.log(response.data[i]);
+          const { name, x, y } = response.data[i];
+          this.createEntity(name, 'player', x, y);
+        }
+      })
+      .then(() => {
+        // initialize game loop and perform first render
+        // after initialization of player/camera/etc
+        console.log('INIT GAME LOOP + FIRST RENDER');
+        this.renderer.addToTicker((delta) => this.gameLoop(delta));
+        this.renderer.render();
+      });
 
     // define events for EventQueue/Reducer
     this.eventQueue.defineEvent('MOVE_ENTITY',
@@ -48,6 +70,7 @@ class Engine {
         if (entityId === 1) {
           // if entity moved is player, move camera also
           this.entityIdMap[0].move(dx, dy);
+          axios.post('/entity', { ...this.entities[1], map: null });
         }
       });
 
@@ -56,8 +79,6 @@ class Engine {
         this.gameMap.set(this.entityIdMap[entityId].x, this.entityIdMap[entityId].y, tile);
         this.renderer.render();
       });
-
-    this.renderer.render();
   }
 
   gameLoop(delta) {
@@ -66,8 +87,15 @@ class Engine {
 
   play(delta) {
     // play state function
+    axios.get('/entity')
+      .then((response) => {
+        if (response.data && response.data.length > 1) {
+          this.updateEntities(response.data);
+        }
+      });
     if (this.eventQueue.length > 0) {
       this.eventQueue.next(delta);
+      console.log('ENTITY COUNT: ', this.entities.length);
       if (this.eventQueue.length < 1) {
         // if event queue is emptied, ie all potential state change is computed, re-render
         this.renderer.update();
@@ -86,6 +114,27 @@ class Engine {
     }
     this.entities.push(createdEntity);
     this.entityIdMap[id] = createdEntity;
+  }
+
+  updateEntities(entityArr) {
+    this.entities = this.entities.slice(0, 2);
+    for (let i = 1; i < entityArr.length; i += 1) {
+      const {
+        name,
+        textureKey,
+        x,
+        y,
+      } = entityArr[i];
+      this.createEntity(name, textureKey, x, y);
+    }
+    this.renderer.render();
+  }
+
+  centerCamera(updateView = true) {
+    this.entities[0].setPos(this.entities[1].x, this.entities[1].y);
+    if (updateView) {
+      this.renderer.update();
+    }
   }
 }
 
