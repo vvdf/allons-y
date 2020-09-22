8057d1f6c7a82c37e7bbe952e203e78fa94f9562
vvdf <t42please@gmail.com>
2020-09-22 03:58:12 -0700

    UPDATE variable name map for consistency

--- client/src/components/Entity.js
+++ client/src/components/Entity.js
@@ -1,21 +1,21 @@
 import { clamp } from './Utility';
 
 class Entity {
-  constructor(name, textureKey, x, y, map, id = 0) {
+  constructor(name, textureKey, x, y, gameMap, id = 0) {
     this.id = id;
     this.name = name;
     this.textureKey = textureKey;
     this.visible = textureKey !== 'blank'; // default to hidden if texture blank
-    if (!Number.isNaN(x) && !Number.isNaN(y) && map) {
+    if (!Number.isNaN(x) && !Number.isNaN(y) && gameMap) {
       this.x = x;
       this.y = y;
-      this.map = map;
+      this.gameMap = gameMap;
     }
   }
 
   move(dx, dy) {
-    this.x = clamp(this.x + dx, 0, this.map.width - 1);
-    this.y = clamp(this.y + dy, 0, this.map.height - 1);
+    this.x = clamp(this.x + dx, 0, this.gameMap.width - 1);
+    this.y = clamp(this.y + dy, 0, this.gameMap.height - 1);
   }
 
   setPos(x, y) {
