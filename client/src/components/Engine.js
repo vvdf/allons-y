e78bc7c91458215d4aa6ebfdf83d896646e3c3bc
vvdf <t42please@gmail.com>
2020-09-23 18:40:12 -0700

    IMPLEMENT better referencing for socket url

--- client/src/components/Engine.js
+++ client/src/components/Engine.js
@@ -71,7 +71,7 @@ class Engine {
         const {
           name, textureKey, x, y, gameMap, id,
         } = this.entities[1];
-        this.sio = new SocketInterface(this.eventQueue, `${window.location}:3001`);
+        this.sio = new SocketInterface(this.eventQueue, `${window.location.hostname}:3001`);
         this.sio.emitOnConnect('gameEvent', { signal: 'NEW_ENTITY', params: [name, textureKey, x, y, gameMap, id] });
       });
 
