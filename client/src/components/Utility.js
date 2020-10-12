c23e2b2cd9eec0e77eef697ced9f4df35041a4a5
vvdf <t42please@gmail.com>
2020-10-11 21:34:37 -0700

    REFACTOR formatNoun for multiword nouns

--- client/src/components/Utility.js
+++ client/src/components/Utility.js
@@ -1,4 +1,11 @@
 // helper functions
 export const clamp = (value, min, max) => Math.max(Math.min(value, max), min);
 
-export const formatNoun = (str) => `${str.substr(0, 1).toUpperCase()}${str.substr(1).toLowerCase()}`;
+export const formatNoun = (str) => {
+  const multipleWords = str.split(' ');
+  let result = '';
+  multipleWords.forEach((word) => {
+    result += `${word.substr(0, 1).toUpperCase()}${word.substr(1).toLowerCase()}`;
+  });
+  return result;
+};
