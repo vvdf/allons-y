b61d99bba8fe062220ef6989c243ae6ccca15d2d
vvdf <t42please@gmail.com>
2020-10-18 23:49:54 -0700

    ADD mild shift animation

--- client/src/components/Animations.js
+++ client/src/components/Animations.js
@@ -28,3 +28,47 @@ export const blinkOut = (delta, spriteRef, delay, callback) => {
     callback();
   }
 };
+
+export const dodgeShift = (delta, spriteRef, delay, displaceX, displaceY, callback) => {
+  if (!{}.hasOwnProperty.call(spriteRef, 'deltas')) {
+    const deltas = {
+      stepX: true,
+      stepY: true,
+      dx: 0,
+      dy: 0,
+      dxEnd: Math.abs(displaceX),
+      dyEnd: Math.abs(displaceY),
+    };
+
+    spriteRef.deltas = deltas;
+  }
+
+  const xDelta = delta / delay;
+  const yDelta = delta / (delay * 2);
+
+  if (spriteRef.deltas.stepX && spriteRef.deltas.dx >= spriteRef.deltas.dxEnd) {
+    // norgmalize the difference if it goes too far
+    spriteRef.deltas.x += spriteRef.deltas.dx - spriteRef.deltas.dxEnd;
+    spriteRef.deltas.stepX = false;
+  } else if (spriteRef.deltas.stepX) {
+    // step animation forward
+    spriteRef.deltas.x += xDelta;
+    spriteRef.deltas.dx += Math.abs(xDelta);
+  }
+
+  if (spriteRef.deltas.stepY && spriteRef.deltas.dy >= spriteRef.deltas.dyEnd) {
+    // norgmalize the difference if it goes too far
+    spriteRef.deltas.y += spriteRef.deltas.dy - spriteRef.deltas.dyEnd;
+    spriteRef.deltas.stepY = false;
+  } else if (spriteRef.deltas.stepY) {
+    // step animation forward
+    spriteRef.deltas.y += yDelta;
+    spriteRef.deltas.dy += Math.abs(yDelta);
+  }
+
+  if (!spriteRef.deltas.stepX && !spriteRef.deltas.stepY) {
+    // if shift is complete on both axis, end animation
+    delete spriteRef.deltas;
+    callback();
+  }
+};
