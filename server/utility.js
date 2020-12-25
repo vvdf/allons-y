b1a4f553c7e35be10c8556dbe8487cb443094835
7FFF00 <t42please@gmail.com>
2020-12-24 20:58:32 -0800

    ADD generation helpers

--- server/utility.js
+++ server/utility.js
@@ -8,12 +8,24 @@ const parseCookies = (cookieString = '') => {
   return result;
 };
 
+const generateID = () => Math.random().toString(36).substring(9);
+
+// TODO replace this placeholder name generator
+const nameBankA = ['Metal', 'Super', 'Guardians', 'Armored', 'Ghost', 'Gun', 'Mystery'];
+const nameBankB = [' Wolf', ' Street', ' of the', ' Core', ' Busters', ' Breakers', ' Team']
+const nameBankC = [' Chaos', ' Fighter', ' Galaxy', ' Raven', ' Live', ' Power', ' Incorporated']
+const nameBankD = [' Division', ' Guild', ' Department', ' Unit', ' Squad', ''];
+const generateName = () => `${nameBankA[rng(0, 6)]}${nameBankB[rng(0, 6)]}` +
+  `${nameBankC[rng(0, 6)]}${nameBankD[rng(0, 5)]}`;
+
 const rng = (min, max) => Math.round(Math.random() * (max - min) + min);
 
 const clamp = (value, min, max) => Math.max(Math.min(value, max), min);
 
 module.exports = {
   parseCookies,
+  generateID,
+  generateName,
   rng,
   clamp,
 };
