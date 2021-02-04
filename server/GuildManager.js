9478dfe1c1af0f9516d2d94d87690f1e82d9ccee
7FFF00 <t42please@gmail.com>
2021-02-04 01:36:18 -0800

    FIX formatting for improved styling

--- server/GuildManager.js
+++ server/GuildManager.js
@@ -3,15 +3,25 @@ const Guild = require('./Guild');
 class GuildManager {
   constructor() {
     this.guildList = {};
+    this.guildLocationList = {};
   }
 
   addGuild(location) {
     const newGuild = new Guild(location);
     this.guildList[newGuild.id] = newGuild;
+    if (!{}.hasOwnProperty.call(this.guildLocationList, location)) {
+      // if location doesn't exist yet, then initialize it
+      this.guildLocationList[location] = {};
+    }
+    this.guildLocationList[location] = newGuild.id;
   }
 
   removeGuild(gid) {
     delete this.guildList[gid];
+  }
+
+  guildExists(location) {
+    return {}.hasOwnProperty.call(this.guildLocationList, location);
   }
 }
 
